1d979e132e1f060524ff4ec3519c6f630053c94a
Zamanbek <zturukbayev888@gmail.com>
2020-11-13 13:06:33 +0600

    Fixed bug with login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.layout')
 
 @section('head_links')
-    <!-- <meta charset="UTF-8">
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
@@ -10,28 +10,35 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
   
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <link href="{{ asset('css/login.css') }}" rel="stylesheet"> -->
+    <link href="{{ asset('css/login.css') }}" rel="stylesheet">
 @endsection
 
 
 @section('content')
 <body style="background-image: url('img/bg-applicant-asia-1__min_.jpg');">  
 
-    <!-- <section id="cover" class="min-vh-100" >
+    <section id="cover" class="min-vh-100" >
         <div id="cover-caption">
             <div class="container ">
                 <div class="row text-white ">
                     <div class="col-md-6-offset-12 mx-auto text-center ">
                         <h4 class="display-4 py-2 text-truncate mb-5 mt-3">Authorization</h4>
                         <div class="px-2">
-                            <form method="POST" action="{{ route('login') }}" class="justify-content-center">
+                            <!-- <form method="POST" action="{{ route('login') }}" class="justify-content-center">
                                 @csrf
                                 
                                 <div class="form-group">
-                                    <input type="text" id="e1" class="form-control" name="email" value="{{ old('email') }}" placeholder="Email Address" required autocomplete="email" autofocus>
+                                    <input type="email" id="e1" class="form-control" name="email" value="{{ old('email') }}" placeholder="Email Address" required autocomplete="email" autofocus>
                                 </div>
                                 <div class="form-group">
                                     <input type="password" class="form-control" placeholder="Password" required autocomplete="current-password">
+                                </div>
+                                <div class="form-group">
+                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+
+                                    <label class="form-check-label" for="remember">
+                                        {{ __('Remember Me') }}
+                                    </label>
                                 </div>
                                 <button type="submit" class="btn btn-outline mt-4 " style="color:#fff; border-color: #fff; ">{{ __('Sign In') }}</button>
                                 @if (Route::has('password.request'))
@@ -40,14 +47,46 @@
                                         {{ __('Forgot Your Password?') }}
                                     </a>
                                 @endif
+                            </form> -->
+
+                            <form method="POST" action="{{ route('login') }}">
+                                @csrf
+
+                                <div class="form-group">
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" placeholder="Email Address" required autocomplete="email" autofocus>  
+                                </div>
+
+                                <div class="form-group">
+                                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" placeholder="Password" required autocomplete="current-password">
+                                </div>
+
+                                <!-- <div class="form-group">
+                                    
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+
+                                        <label class="form-check-label" for="remember">
+                                            {{ __('Remember Me') }}
+                                        </label>
+                                    </div>
+                                    
+                                </div> -->
+
+                                <button type="submit" class="btn btn-outline mt-4 " style="color:#fff; border-color: #fff; ">{{ __('Sign In') }}</button>
+                                        @if (Route::has('password.request'))
+                                            <br/>
+                                            <a class="btn btn-link" href="{{ route('password.request') }}" style="color:white;">
+                                                {{ __('Forgot Your Password?') }}
+                                            </a>
+                                        @endif
                             </form>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </section> -->
-<div class="container">
+    </section>
+<!-- <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -115,6 +154,6 @@
             </div>
         </div>
     </div>
-</div>
+</div> -->
 </body>
 @endsection
